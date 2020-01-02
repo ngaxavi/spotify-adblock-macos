fa745ced16385a3d85e192d32076617c859397e1
AnanthVivekanand <avivek3021@gmail.com>
2020-01-01 17:41:07 -1000

    Make it so.

---
+++ blacklist.h
@@ -0,0 +1,4 @@
+static const char *blacklist[2] = {
+    "https://spclient.wg.spotify.com/ads/*",
+    "https://spclient.wg.spotify.com/ad-logic/*",
+};
---
+++ whitelist.h
@@ -0,0 +1,36 @@
+static const char *whitelist[32] = {
+    "ap.spotify.com", // audio (access point)
+    "*.ap.spotify.com", // resolved access points
+    "apresolve.spotify.com", // access point resolving
+    "audio-fa.scdn.co", // audio
+    "audio-sp-*.pscdn.co", // audio
+    "audio-sp-*.spotifycdn.net", // audio
+    "audio4-ak.spotify.com.edgesuite.net", // audio
+    "docker.for.mac.localhost", // docker audio
+    "heads4-ak-spotify-com.akamaized.net", // audio (heads)
+    "host.docker.internal", // docker display
+    "audio4-fa.scdn.co", // audio
+    "audio.simplecast.com", // podcasts
+    "charts-images.scdn.co", // charts images
+    "content.production.cdn.art19.com", // podcasts
+    "daily-mix.scdn.co", // daily mix images
+    "dailymix-images.scdn.co", // daily mix images
+    "dealer.spotify.com", // discord rich presence
+    "heads-fa.scdn.co", // audio (heads)
+    "i.scdn.co", // cover art
+    "image-upload.spotify.com", // image uploading
+    "lineup-images.scdn.co", // playlists lineup images
+    "media.simplecast.com", // podcasts
+    "merch-img.scdn.co", // merch images
+    "mosaic.scdn.co", // playlist mosaic images
+    "newjams-images.scdn.co", // release radar images
+    "o.scdn.co", // cover art
+    "pl.scdn.co", // playlist images
+    "profile-images.scdn.co", // artist profile images
+    "rss.art19.com", // podcasts
+    "seeded-session-images.scdn.co", // radio images
+    "spclient.wg.spotify.com", // ads/tracking (blocked in blacklist), radio, recently played, friend activity,...
+    "t.scdn.co", // background images
+    "thisis-images.scdn.co", // 'this is' playlists images
+    "video-fa.scdn.co", // videos
+};
